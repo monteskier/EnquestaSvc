a32ea0520e83157d23970d5f2f72b36bfaf3b74b
monteskier <jmontoliu7@gmail.com>
2017-12-03 16:34:08 +0100

    Desa be els resultats, falta provar els comentaris

--- models/resultats.js
+++ models/resultats.js
@@ -1,6 +1,6 @@
 function Resultats(){
-}
-Resultats.prototype.reset = function() {
+
+this.reset = function() {
 
   this.vegades = 0;
   this.pregunta1 = {};
@@ -9,8 +9,8 @@ Resultats.prototype.reset = function() {
   this.pregunta4 = {};
   this.pregunta5 = {};
   this.pregunta6 = {};
-  this.pregunta7 = [];
-
+  this.pregunta7 = {};
+  this.pregunta8 = {}
 
   this.pregunta1.desc = "Coneix la iniciativa de conseció de la Medalla de Sant Vicenç?";
   this.pregunta2.desc = "Ha participat presentant candidats o bé votant per un d'ells?";
@@ -18,110 +18,179 @@ Resultats.prototype.reset = function() {
   this.pregunta4.desc = "I el guanyador/a de edició actual, de 2017?";
   this.pregunta5.desc = "Voldria participar activament en la Comissió creada a l'efcte:";
   this.pregunta6.desc = "Creu que és una iniciativa que pot aportar valor a la història del municipi?";
-  this.pregunta7.desc = "observacions/suggeriments";
-};
+  this.pregunta7.desc = "Li sembla que el procés de concessió és el correcte? ";
+  this.pregunta8.desc = "observacions/suggeriments";
 
-//Pregunta 1 getter/setter
+  this.pregunta1.si = 0;
+  this.pregunta1.no = 0;
 
-Resultats.prototype.getVotsP1 = function(){
-  var result = {};
-  result.si = this.pregunta1.si;
-  result.no = this.pregunta1.no;
-  return result;
-}
-Resultats.prototype.setVotsP1 = function (result) {
-  if(resilt.op1 == "Si"){
-    this.pregunta1.si = this.pregunta1.si + 1;
-  }else if (result.op1=="No"){
-    this.pregunta1.no = this.pregunta1.no + 1;
+  this.pregunta2.si = 0;
+  this.pregunta2.no = 0;
+
+  this.pregunta3.si = 0;
+  this.pregunta3.no = 0;
+
+  this.pregunta4.si = 0;
+  this.pregunta4.no = 0;
+
+  this.pregunta5.si = 0;
+  this.pregunta5.no = 0;
+
+
+  this.pregunta6.valor = 0;
+
+  this.pregunta7.valor = 0;
+
+
+  this.pregunta8.comentaris = [];
+
+  //Vegades Setter/getter
+
+  this.getVegades = function () {
+    return this.vegades;
+  };
+  this.addVegades = function(){
+    this.vegades++;
+  }
+  //Pregunta 1 getter/setter
+
+  this.getVotsP1 = function(){
+    var result = {};
+    result.si = this.pregunta1.si;
+    result.no = this.pregunta1.no;
+    return result;
+  }
+
+  this.setVotsP1 = function (result, antic) {
+    //crear condicio de si hi ha historic, poder recuperar les anterios
+    //votacions dintre de un if antic != 0
+
+    if(antic!=0){
+      this.pregunta1.si = antic.si;
+      this.pregunta1.no = antic.no;
+    }
+    if(result== "Si"){
+      this.pregunta1.si = this.pregunta1.si + 1;
+    }else if (result=="No"){
+      this.pregunta1.no = this.pregunta1.no + 1;
+    }
+  }
+  //Pregunta 2 getter/setter
+
+  this.getVotsP2 = function(){
+    var result = {};
+    result.si = this.pregunta2.si;
+    result.no = this.pregunta2.no;
+    return result;
+  }
+  this.setVotsP2 = function (result, antic) {
+    if(antic!=0){
+      this.pregunta2.si = antic.si;
+      this.pregunta2.no = antic.no;
+    }
+    if(result == "Si"){
+      this.pregunta2.si = this.pregunta2.si + 1;
+    }else if (result=="No"){
+      this.pregunta2.no = this.pregunta2.no + 1;
+    }
+  }
+  //Pregunta 3 getter/setter
+
+  this.getVotsP3 = function(){
+    var result = {};
+    result.si = this.pregunta3.si;
+    result.no = this.pregunta3.no;
+    return result;
+  }
+  this.setVotsP3 = function (result, antic) {
+    if(antic!=0){
+      this.pregunta3.si = antic.si;
+      this.pregunta3.no = antic.no;
+    }
+    if(result == "Si"){
+      this.pregunta3.si = this.pregunta3.si + 1;
+    }else if (result=="No"){
+      this.pregunta3.no = this.pregunta3.no + 1;
+    }
+  }
+
+  //Pregunta 4 getter/setter
+
+  this.getVotsP4 = function(){
+    var result = {};
+    result.si = this.pregunta4.si;
+    result.no = this.pregunta4.no;
+    return result;
+  }
+  this.setVotsP4 = function (result, antic) {
+    if(antic!=0){
+      this.pregunta4.si = antic.si;
+      this.pregunta4.no = antic.no;
+    }
+    if(result== "Si"){
+      this.pregunta4.si = this.pregunta4.si + 1;
+    }else if (result=="No"){
+      this.pregunta4.no = this.pregunta4.no + 1;
+    }
+  }
+  //Pregunta 5 getter/setter
+
+  this.getVotsP5 = function(){
+    var result = {};
+    result.si = this.pregunta5.si;
+    result.no = this.pregunta5.no;
+    return result;
+  }
+  this.setVotsP5 = function (result, antic) {
+    if(antic!=0){
+      this.pregunta5.si = antic.si;
+      this.pregunta5.no = antic.no;
+    }
+
+    if(result == "Si"){
+      this.pregunta5.si = this.pregunta5.si + 1;
+    }else if (result=="No"){
+      this.pregunta5.no = this.pregunta5.no + 1;
+    }
+  }
+
+  //Pregunta 6 getter/setter
+
+  this.getVotsP6 = function(){
+    return this.pregunta6.valor;
+  }
+
+  this.setVotsP6 = function (valor, antic) {
+    if(antic!=0){
+      this.pregunta6.valor = antic.valor;
+    }
+    this.pregunta6.valor = this.pregunta6.valor + valor
+  }
+  //Pregunta 7 getter/setter
+  this.getVotsP7 = function(){
+    return this.pregunta7.valor;
+  }
+
+  this.setVotsP7 = function (valor, antic) {
+    if(antic!=0){
+      this.pregunta7.valor = antic.valor;
+    }
+    this.pregunta7.valor = this.pregunta7.valor + valor
+  }
+
+  //Pregunta 8 getter/setter
+
+  this.getComentaris = function () {
+    return this.pregunta8.comentaris;
+  };
+  this.setComentaris = function(result, antic){
+    if(antic.comentaris.length!=0){
+      antic.comentaris.forEach(function(element){
+          this.pregunta8.comentaris.push(element);
+      });
+    }*/
+    this.pregunta8.comentaris.push(result);
   }
 }
-
-//Pregunta 2 getter/setter
-
-Resultats.prototype.getVotsP2 = function(){
-  var result = {};
-  result.si = this.pregunta2.si;
-  result.no = this.pregunta2.no;
-  return result;
 }
-Resultats.prototype.setVotsP2 = function (result) {
-  if(resilt.op2 == "Si"){
-    this.pregunta2.si = this.pregunta2.si + 1;
-  }else if (result.op2=="No"){
-    this.pregunta2.no = this.pregunta2.no + 1;
-  }
-}
-
-//Pregunta 3 getter/setter
-
-Resultats.prototype.getVotsP3 = function(){
-  var result = {};
-  result.si = this.pregunta3.si;
-  result.no = this.pregunta3.no;
-  return result;
-}
-Resultats.prototype.setVotsP3 = function (result) {
-  if(resilt.op3 == "Si"){
-    this.pregunta3.si = this.pregunta3.si + 1;
-  }else if (result.op3=="No"){
-    this.pregunta3.no = this.pregunta3.no + 1;
-  }
-}
-
-//Pregunta 4 getter/setter
-
-Resultats.prototype.getVotsP4 = function(){
-  var result = {};
-  result.si = this.pregunta4.si;
-  result.no = this.pregunta4.no;
-  return result;
-}
-Resultats.prototype.setVotsP4 = function (result) {
-  if(resilt.op4 == "Si"){
-    this.pregunta4.si = this.pregunta4.si + 1;
-  }else if (result.op4=="No"){
-    this.pregunta4.no = this.pregunta4.no + 1;
-  }
-}
-//Pregunta 5 getter/setter
-
-Resultats.prototype.getVotsP5 = function(){
-  var result = {};
-  result.si = this.pregunta5.si;
-  result.no = this.pregunta5.no;
-  return result;
-}
-Resultats.prototype.setVotsP5 = function (result) {
-  if(resilt.op5 == "Si"){
-    this.pregunta5.si = this.pregunta5.si + 1;
-  }else if (result.op5=="No"){
-    this.pregunta5.no = this.pregunta5.no + 1;
-  }
-}
-
-//Pregunta 6 getter/setter
-
-Resultats.prototype.getVotsP6 = function(){
-  var result = {};
-  result.si = this.pregunta6.si;
-  result.no = this.pregunta6.no;
-  return result;
-}
-Resultats.prototype.setVotsP6 = function (result) {
-  if(resilt.op6 == "Si"){
-    this.pregunta6.si = this.pregunta6.si + 1;
-  }else if (result.op6=="No"){
-    this.pregunta6.no = this.pregunta6.no + 1;
-  }
-}
-//Pregunta 7 getter/setter
-
-Resultats.prototype.getComentaris = function () {
-  return this.pregunta7.comentaris;
-};
-Resultats.prototype.setComentaris = function(comentaris){
-  this.pregunta7.comentaris.push = comentaris;
-}
-
 module.exports = Resultats;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var ResultatsObj = require("../models/resultats.js");
+var ObjectID = require('mongodb').ObjectID;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,16 +11,27 @@ router.post('/enviar', function(req, res, next){
   var db = req.db;
   var collection = db.get('resultats');
 
-  console.log(req.body);
   collection.findOne({},{},function(e, docs){
     if(docs){
       console.log("Hi han dades desades");
-      return true;
+      next(actualitzar(req, res, next));
+      res.json({"msg":"Gràcies per participar"});
     }else{
       console.log("No Hi han dades desades");
-
+      var post = (req.body);
+      console.log("el post te:"+post.op1);
       var Resultats = new ResultatsObj();
       Resultats.reset();
+      Resultats.addVegades();
+      Resultats.setVotsP1(post.op1, 0);
+      Resultats.setVotsP2(post.op2, 0);
+      Resultats.setVotsP3(post.op3, 0);
+      Resultats.setVotsP4(post.op4, 0);
+      Resultats.setVotsP5(post.op5, 0);
+      Resultats.setVotsP6(parseInt(post.op6, 0));
+      Resultats.setVotsP7(parseInt(post.op7, 0));
+      Resultats.setComentaris(post.op8, 0);
+
       collection.insert(Resultats, function(e, docs){
         if(e){
           res.json({"msg":e});
@@ -39,5 +51,36 @@ router.post('/enviar', function(req, res, next){
     }
   });*/
 });
+function actualitzar(req, res, next){
+
+  var db = req.db;
+  var collection = db.get('resultats');
+  var post = req.body;
+
+
+  collection.findOne({},{}, function(e, docs, next){
+    var objectId = new ObjectID(docs._id);
+    console.log("DADES DE DOCS"+docs.pregunta1.desc);
+    var Resultats = new ResultatsObj();
+    Resultats.reset();
+    Resultats.addVegades();
+    Resultats.setVotsP1(post.op1, docs.pregunta1); //sha de pasar el antic valor i sumar-ho abans de incrementar
+    Resultats.setVotsP2(post.op2, docs.pregunta2);
+    Resultats.setVotsP3(post.op3, docs.pregunta3);
+    Resultats.setVotsP4(post.op4, docs.pregunta4);
+    Resultats.setVotsP5(post.op5, docs.pregunta5);
+    Resultats.setVotsP6(parseInt(post.op6),parseInt(docs.pregunta6.valor));
+    Resultats.setVotsP7(parseInt(post.op7), parseInt(docs.pregunta7.valor));
+    Resultats.setComentaris(post.op8, docs.pregunta8);
+
+    collection.update({"_id": objectId},Resultats, function(e, docssub){
+      if(e){
+        res.json({"msg":e});
+      }else{
+        res.json({"msg":"Gràcies per participar"});
+      }
+    });
+  });
+}
 
 module.exports = router;
